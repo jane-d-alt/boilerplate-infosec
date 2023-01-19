fcb1215aba9a78fe5f6648601527135c29b38a74
jane-d-alt <51848939+jane-d-alt@users.noreply.github.com>
2023-01-19 19:09:02 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-let helmet = require("helmet");
+let helmet = require('helmet');
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
@@ -9,68 +9,16 @@ app.use(helmet.ieNoOpen());
 let ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
+
+/*the following can be implemented with a helmet configuration object
 app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}}));
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}}));*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+//example:
+app.use(helmet({
+  noCache(),
+  contentSecurityPolicy: {
+    defaultSrc: ["'self'"], 
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
