63d9d4431b722bd9d33d340165d05c5c4b37f1ca
jane-d-alt <51848939+jane-d-alt@users.noreply.github.com>
2023-01-19 18:48:55 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 let ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
 
 
 
